baae05fba32f83cd85a238adbf7205f78cc2023d
hortn <74456584+hortn@users.noreply.github.com>
2022-11-25 14:24:39 +0500

    Add files via upload

---
+++ Globals.ps1
@@ -0,0 +1,67 @@
+﻿function Get-ScriptDirectory
+{
+
+	[OutputType([string])]
+	param ()
+	if ($null -ne $hostinvocation)
+	{
+		Split-Path $hostinvocation.MyCommand.path
+	}
+	else
+	{
+		Split-Path $script:MyInvocation.MyCommand.Path
+	}
+}
+
+[string]$ScriptDirectory = Get-ScriptDirectory
+
+
+function connect
+{
+	param ($PC_Name, $Vnc = '')
+	Start-Process -FilePath "C:\Program Files (x86)\SolarWinds\Dameware Remote Support\dwrcc.exe" -ArgumentList "-c: -h: -m:$PC_Name $Vnc -x:"
+	
+}
+
+
+function Update-ComboBox
+{
+
+	param
+	(
+		[Parameter(Mandatory = $true)]
+		[ValidateNotNull()]
+		[System.Windows.Forms.ComboBox]$ComboBox,
+		[Parameter(Mandatory = $true)]
+		[ValidateNotNull()]
+		$Items,
+		[Parameter(Mandatory = $false)]
+		[string]$DisplayMember,
+		[switch]$Append
+	)
+	
+	if (-not $Append)
+	{
+		$ComboBox.Items.Clear()
+	}
+	
+	if ($Items -is [Object[]])
+	{
+		$ComboBox.Items.AddRange($Items)
+	}
+	elseif ($Items -is [System.Collections.IEnumerable])
+	{
+		$ComboBox.BeginUpdate()
+		foreach ($obj in $Items)
+		{
+			$ComboBox.Items.Add($obj)
+		}
+		$ComboBox.EndUpdate()
+	}
+	else
+	{
+		$ComboBox.Items.Add($Items)
+	}
+	
+	$ComboBox.DisplayMember = $DisplayMember
+}
